fd8bc3fc7cd1294b3aaf3e29563ab265a1fcbc38
Phuoc Nhat <doanphuocnhat205@gmail.com>
2023-07-26 20:55:58 +0700

    26/07

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,24 +1,29 @@
 "use client";
 
-import { AiOutlineMenu } from "react-icons/ai";
 import { useCallback, useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
 import { signOut } from "next-auth/react";
-import { SafeUser } from "@/app/types";
-import Avatar from "../Avatar";
-import MenuItem from "./MenuItem";
+import { useRouter } from "next/navigation";
 
-import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useRentModal from "@/app/hooks/useRentModal";
+import { SafeUser } from "@/app/types";
+
+import MenuItem from "./MenuItem";
+import Avatar from "../Avatar";
 
 interface UserMenuProps {
     currentUser?: SafeUser | null;
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
-    const registerModal = useRegisterModal();
+    const router = useRouter();
+
     const loginModal = useLoginModal();
+    const registerModal = useRegisterModal();
     const rentModal = useRentModal();
+
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleOpen = useCallback(() => {
@@ -29,21 +34,47 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
         if (!currentUser) {
             return loginModal.onOpen();
         }
+
         rentModal.onOpen();
-    }, [currentUser, loginModal, rentModal]);
+    }, [loginModal, rentModal, currentUser]);
 
     return (
         <div className="relative">
             <div className="flex flex-row items-center gap-3">
                 <div
                     onClick={onRent}
-                    className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
+                    className="
+            hidden
+            md:block
+            text-sm 
+            font-semibold 
+            py-3 
+            px-4 
+            rounded-full 
+            hover:bg-neutral-100 
+            transition 
+            cursor-pointer
+          "
                 >
-                    App app
+                    Airbnb your home
                 </div>
                 <div
                     onClick={toggleOpen}
-                    className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+                    className="
+          p-4
+          md:py-1
+          md:px-2
+          border-[1px] 
+          border-neutral-200 
+          flex 
+          flex-row 
+          items-center 
+          gap-3 
+          rounded-full 
+          cursor-pointer 
+          hover:shadow-md 
+          transition
+          "
                 >
                     <AiOutlineMenu />
                     <div className="hidden md:block">
@@ -52,42 +83,58 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                 </div>
             </div>
             {isOpen && (
-                <div className="absolute rounded-xl shadow-md w-[30vw] md:w-5/6 bg-white overflow-hidden right-0 top-12 text-sm">
+                <div
+                    className="
+            absolute 
+            rounded-xl 
+            shadow-md
+            w-[40vw]
+            md:w-3/4 
+            bg-white 
+            overflow-hidden 
+            right-0 
+            top-12 
+            text-sm
+          "
+                >
                     <div className="flex flex-col cursor-pointer">
                         {currentUser ? (
                             <>
-                                <MenuItem onClick={() => {}} label="My trips" />
                                 <MenuItem
-                                    onClick={() => {}}
+                                    label="My trips"
+                                    onClick={() => router.push("/trips")}
+                                />
+                                <MenuItem
                                     label="My favorites"
+                                    onClick={() => router.push("/favorites")}
                                 />
                                 <MenuItem
-                                    onClick={() => {}}
                                     label="My reservations"
+                                    onClick={() => router.push("/reservations")}
                                 />
                                 <MenuItem
-                                    onClick={() => {}}
                                     label="My properties"
+                                    onClick={() => router.push("/properties")}
                                 />
                                 <MenuItem
+                                    label="Airbnb your home"
                                     onClick={rentModal.onOpen}
-                                    label="Airbnb my home"
                                 />
                                 <hr />
                                 <MenuItem
-                                    onClick={() => signOut()}
                                     label="Logout"
+                                    onClick={() => signOut()}
                                 />
                             </>
                         ) : (
                             <>
                                 <MenuItem
-                                    onClick={loginModal.onOpen}
                                     label="Login"
+                                    onClick={loginModal.onOpen}
                                 />
                                 <MenuItem
+                                    label="Sign up"
                                     onClick={registerModal.onOpen}
-                                    label="SignUp"
                                 />
                             </>
                         )}
